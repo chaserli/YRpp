5d0f1f8cca4a6632b7c665320a5f3507a8a64cc9
Belonit <54427022+Belonit@users.noreply.github.com>
2024-01-21 09:54:33 +0300

    Update EventClass.h

--- EventClass.h
+++ EventClass.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <Helpers/CompileTime.h>
-
+#include <ScenarioClass.h>
 #include <TargetClass.h>
 #include <Unsorted.h>
 
@@ -58,100 +57,6 @@ enum class EventType : unsigned char
 };
 
 #pragma pack(push, 1)
-union EventData
-{
-	EventData()
-	{
-
-	}
-
-	struct
-	{
-		char Data[104];
-	} SpaceGap; // Just a space gap to align the struct
-
-	struct
-	{
-		int ID; // Anim ID
-		int AnimOwner; // House ID
-		CellStruct Location;
-	} Animation;
-
-	struct
-	{
-		unsigned int CRC;
-		unsigned short CommandCount;
-		unsigned char Delay;
-	} FrameInfo;
-
-	struct
-	{
-		TargetClass Whom;
-	} Target;
-
-	struct
-	{
-		TargetClass Whom;
-		unsigned char Mission; // Mission but should be byte
-		char _gap_;
-		TargetClass Target;
-		TargetClass Destination;
-		TargetClass Follow;
-		bool IsPlanningEvent;
-	} MegaMission;
-
-	struct
-	{
-		TargetClass Whom;
-		unsigned char Mission;
-		TargetClass Target;
-		TargetClass Destination;
-		int Speed;
-		int MaxSpeed;
-	} MegaMission_F; // Seems unused in YR?
-
-	struct
-	{
-		int RTTI_ID;
-		int Heap_ID;
-		int IsNaval;
-	} Production;
-
-	struct
-	{
-		int Unknown_0;
-		long long Data;
-		int Unknown_C;
-	} Unknown_LongLong;
-
-	struct
-	{
-		int Unknown_0;
-		int Unknown_4;
-		int Data;
-		int Unknown_C;
-	} Unknown_Tuple;
-
-	struct
-	{
-		AbstractType RTTIType;
-		int HeapID;
-		int IsNaval;
-		CellStruct Location;
-	} Place;
-
-	struct
-	{
-		int ID;
-		CellStruct Location;
-	} SpecialPlace;
-
-	struct
-	{
-		AbstractType RTTIType;
-		int ID;
-	} Specific;
-};
 
 class EventClass;
 
@@ -169,16 +74,17 @@ public:
 class EventClass
 {
 public:
-	static constexpr reference<const char*, 0x82091C, 47> const EventNames{};
+	static constexpr reference<const char*, 0x0082091C, 47> const EventNames {};
 
-	static constexpr reference<EventList<0x80>, 0xA802C8> OutList{};
-	static constexpr reference<EventList<0x4000>, 0x8B41F8> DoList{};
+	static constexpr reference<EventList<0x80>, 0x00A802C8> OutList {};
+	static constexpr reference<EventList<0x4000>, 0x008B41F8> DoList {};
+
 	// If the event is a MegaMission, then add it to this list
-	static constexpr reference<EventList<0x100>, 0xA83ED0> MegaMissionList{};
+	static constexpr reference<EventList<0x100>, 0x00A83ED0> MegaMissionList {};
 
 	// this points to CRCs from 0x100 last frames
-	static constexpr reference<DWORD, 0xB04474, 256> const LatestFramesCRC{};
-	static constexpr reference<DWORD, 0xAC51FC> const CurrentFrameCRC{};
+	static constexpr reference<DWORD, 0x00B04474, 256> const LatestFramesCRC {};
+	static constexpr reference<DWORD, 0x00AC51FC> const CurrentFrameCRC {};
 
 	static bool AddEvent(const EventClass& event)
 	{
@@ -190,7 +96,9 @@ public:
 		OutList->Timings[OutList->Tail] = static_cast<int>(Imports::TimeGetTime()());
 
 		++OutList->Count;
-		OutList->Tail = (OutList->Tail + 1) & 127;
+		OutList->Tail = (LOBYTE(OutList->Tail) + 1) & 127;
+
+		return true;
 	}
 
 	// Special
@@ -272,7 +180,7 @@ public:
 	}
 
 	// Address Change
-	explicit EventClass(int houseIndex, void*/*IPAddressClass*/ ip, char unknown_0)
+	explicit EventClass(int houseIndex, void* /*IPAddressClass*/ ip, char unknown_0)
 	{
 		JMP_THIS(0x4C6C50);
 	}
@@ -286,21 +194,288 @@ public:
 	{
 		if (this != &another)
 			memcpy(this, &another, sizeof(*this));
+
 		return *this;
 	}
-
-	EventType Type;
-	bool IsExecuted;
-	char HouseIndex; // '-1' stands for not a valid house
-	unsigned int Frame; // 'Frame' is the frame that the command should execute on.
-
-	EventData Data;
 
 	bool operator==(const EventClass& q) const
 	{
 		return memcmp(this, &q, sizeof(q)) == 0;
 	};
+
+	// ========================
+	EventType Type;
+	bool IsExecuted;
+	char HouseIndex; // '-1' stands for not a valid house
+	unsigned int Frame; // 'Frame' is the frame that the command should execute on.
+
+	union
+	{
+		char DataBuffer[104];
+
+		struct EMPTY
+		{ } Empty;
+
+		struct POWERON
+		{
+			TargetClass Target;
+		} Poweron;
+
+		struct POWEROFF
+		{
+			TargetClass Target;
+		} Poweroff;
+
+		struct ALLY
+		{
+			int HouseID;
+		} Ally;
+
+		struct MEGAMISSION
+		{
+			TargetClass Whom;
+			unsigned char Mission;
+			char _gap_;
+			TargetClass Target;
+			TargetClass Destination;
+			TargetClass Follow;
+			bool IsPlanningEvent;
+		} MegaMission;
+
+		struct MEGAMISSION_F
+		{
+			TargetClass Whom;
+			unsigned char Mission;
+			TargetClass Target;
+			TargetClass Destination;
+			int Speed;
+			int MaxSpeed;
+		} MegaMissionF;
+
+		struct IDLE
+		{
+			TargetClass Whom;
+		} Idle;
+
+		struct SCATTER
+		{
+			TargetClass Whom;
+		} Scatter;
+
+		struct DESTRUCT
+		{ } Destruct;
+
+		struct DEPLOY
+		{
+			TargetClass Whom;
+		} Deploy;
+
+		struct DETONATE
+		{
+			TargetClass Whom;
+		} Detonate;
+
+		struct PLACE
+		{
+			AbstractType RTTIType;
+			int HeapID;
+			int IsNaval;
+			CellStruct Location;
+		} Place;
+
+		struct OPTIONS
+		{ } Options;
+
+		struct GAMESPEED
+		{
+			int GameSpeed;
+		} GameSpeed;
+
+		// This event starts production of the specified object type. The house can
+		// determine from the type and ID value, what object to begin production on and
+		// what factory to use.
+		struct PRODUCE
+		{
+			AbstractType RTTIType;
+			int HeapID;
+			int IsNaval;
+		} Produce;
+
+		struct SUSPEND
+		{
+			AbstractType RTTIType;
+			int HeapID;
+			int IsNaval;
+		} Suspend;
+
+		// This event is generated when the player cancels production of the specified
+		// object type. From the object type, the exact factory can be inferred.
+		struct ABANDON
+		{
+			AbstractType RTTIType;
+			int HeapID;
+			int IsNaval;
+		} Abandon;
+
+		// Toggles the primary factory state of the specified building.
+		struct PRIMARY
+		{
+			TargetClass Whom;
+
+		} Primary;
+
+		struct SPECIAL_PLACE
+		{
+			int ID;
+			CellStruct Location;
+		} SpecialPlace;
+
+		struct EXIT
+		{ } Exit;
+
+		struct ANIMATION
+		{
+			int AnimID;
+			int HouseID;
+			Point2D Location;
+		} Animation;
+
+		// Starts or stops repair on the specified object. This event is triggered by the
+		// player clicking the repair wrench on a building.
+		struct REPAIR
+		{
+			TargetClass Whom;
+		} Repair;
+
+		// Tells a building/unit to sell. This event is triggered by the player clicking the
+		// sell animating cursor over the building or unit.
+		struct SELL
+		{
+			TargetClass Whom;
+		} Sell;
+
+		// Used to sell walls
+		struct SELLCELL
+		{
+			CellStruct Location;
+		} SellCell;
+
+		// Update the special control flags. This is necessary so that in a multiplayer
+		// game, all machines will agree on the rules. If these options change during
+		// game play, then all players are informed that options have changed.
+		struct SPECIAL
+		{
+			ScenarioFlags SpecialFlags;
+		} Special;
+
+		struct FRAMESYNC
+		{ } FrameSync;
+
+		struct MESSAGE
+		{ } Message;
+
+		struct RESPONSE_TIME
+		{
+			char unknown;
+		} ResponseTime;
+
+		struct FRAMEINFO
+		{
+			unsigned int CRC;
+			unsigned short CommandCount;
+			unsigned char Delay;
+		} FrameInfo;
+
+		struct SAVEGAME
+		{ } SaveGame;
+
+		// Update the archive target for this building.
+		struct ARCHIVE
+		{
+			TargetClass Whom1;
+			TargetClass Whom2;
+		} Archive;
+
+		struct ADDPLAYER
+		{
+			void* unknownPointer;
+		} AddPlayer;
+
+		struct TIMING
+		{
+			unsigned short RequestedFPS;
+			unsigned short MaxAhead;
+			unsigned char FrameSendRate;
+		} Timing;
+
+		struct PROCESS_TIME
+		{
+			unsigned short Time;
+		} ProcessTime;
+
+		struct PAGEUSER
+		{ } PageUser;
+
+		struct REMOVEPLAYER
+		{
+			int HouseID;
+		} RemovePlayer;
+
+		struct LATENCYFUDGE
+		{
+			int LatencyFudge;
+		} LatencyFudge;
+
+		struct MEGAFRAMEINFO
+		{
+			char Unknown[104];
+		} MegafFameInfo;
+
+		struct PACKETTIMING
+		{
+			char Unknown[64];
+		} PacketTiming;
+
+		struct ABOUTTOEXIT
+		{ } AboutToExit;
+
+		struct FALLBACKHOST
+		{
+			int FallbackHost;
+		} FallbackHost;
+
+		struct ADDRESSCHANGE
+		{
+			char PlayerID;
+			DWORD Address;
+		} AddressChange;
+
+		struct PLANCONNECT
+		{
+			TargetClass Target1;
+			TargetClass Target2;
+		} PlanConnect;
+
+		struct PLANCOMMIT
+		{ } PlanCommit;
+
+		struct PLANNODEDELETE
+		{
+			TargetClass Target;
+		} PlanNodeDelete;
+
+		struct ALLCHEER
+		{
+			char Unknown[4];
+		} AllCheer;
+
+		struct ABANDON_ALL
+		{
+			char Unknown[12];
+		} AbandonAll;
+	};
 };
 #pragma pack(pop)
 
 static_assert(sizeof(EventClass) == 111);
+static_assert(offsetof(EventClass, DataBuffer) == 7);
