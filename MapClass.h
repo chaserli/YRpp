6455396c852421cdce9c671e4ef3d0b901dac05f
Starkku <starkku.ppmod@gmail.com>
2024-06-19 18:49:22 +0300

    Move LogicClass instance reference under LogicClass from MapClass

--- MapClass.h
+++ MapClass.h
@@ -151,6 +151,8 @@ public:
 class LogicClass : public LayerClass
 {
 public:
+	static constexpr reference<LogicClass, 0x87F778u> const Instance {};
+
 	virtual bool AddObject(ObjectClass* pObject, bool sorted) override
 		{ JMP_THIS(0x55BAA0); }
 
@@ -173,8 +175,6 @@ public:
 	static constexpr reference<CellClass, 0xABDC50u> const InvalidCell{};
 
 	static const int MaxCells = 0x40000;
-
-	static constexpr reference<LogicClass, 0x87F778u> const Logics{};
 
 	// this actually points to 5 vectors, one for each layer
 	static constexpr reference<LayerClass, 0x8A0360u, 5u> const ObjectsInLayers{};
