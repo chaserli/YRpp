99d2a164e4fbdf9e9b55a1bfc7573b719423c093
Starkku <starkku.ppmod@gmail.com>
2024-10-13 22:17:35 +0300

    Update TunnelLocomotionClass
    - Add static ref to value used for underground movement speed

--- TunnelLocomotionClass.h
+++ TunnelLocomotionClass.h
@@ -8,6 +8,7 @@ class __declspec(align(4)) NOVTABLE TunnelLocomotionClass : public LocomotionCla
 {
 public:
 	static constexpr uintptr_t ILocoVTable = 0x7F5A24;
+	static constexpr reference<double, 0x7F5B20u> const TunnelMovementSpeed {};
 
 	enum State
 	{
